608d3ad881478268c063c762aa1d1ea57cb83409
alwaysaskafter <779647751@qq.com>
2020-08-15 11:10:35 +0800

    处理错误

--- view-exam/src/view/main/Main.tsx
+++ view-exam/src/view/main/Main.tsx
@@ -2,11 +2,8 @@ import React from 'react';
 import { useObserver } from 'mobx-react-lite'
 import styled from 'styled-components';
 import MenuSider from '../../components/MenuSide/MenuSider';
-<<<<<<< HEAD
 import HeaderBar from '../../components/HeaderBar/Header'
-=======
 import Roterview from '../../router/RouterView'
->>>>>>> suleiya
 import useStore from '../../context/useStore';
 import { Layout } from 'antd';
 const { Header, Sider, Content } = Layout;
@@ -15,6 +12,8 @@ export default function Main(props: any) {
   const { MainStore } = useStore();
   MainStore.initAction();
   console.log(MainStore.MenuList)
+  console.log(props )
+
   return useObserver(() => <div className="Main"  >
     <Layout>
       <Header style={{ background: '#fff' }}>
@@ -22,12 +21,7 @@ export default function Main(props: any) {
       </Header>
       <Layout>
         <Sider style={{ background: '#232A41' }} ><MenuSider></MenuSider></Sider>
-<<<<<<< HEAD
-        <Content>Content</Content>
-=======
-        <Sider ><MenuSider></MenuSider></Sider>
         <Content><Roterview routes={props.routes} /></Content>
->>>>>>> suleiya
       </Layout>
     </Layout>
   </div>)
