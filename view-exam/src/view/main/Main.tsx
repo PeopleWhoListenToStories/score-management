5f4ae170ef233eb607c53429a3e0446ddb063f74
康泽雅 <1746607503@qq.com>
2020-08-15 11:35:20 +0800

    8.15

--- view-exam/src/view/main/Main.tsx
+++ view-exam/src/view/main/Main.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useObserver } from 'mobx-react-lite'
+import { useObserver } from 'mobx-react-lite' 
 import styled from 'styled-components';
 import MenuSider from '../../components/MenuSide/MenuSider';
 import HeaderBar from '../../components/HeaderBar/Header'
