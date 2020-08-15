2a5df3e2adecd7cbfed902b1a6c794385e7c0297
zhengqiyue <2535250538@qq.com>
2020-08-15 10:15:30 +0800

    郑其月

--- view-exam/src/view/main/Main.tsx
+++ view-exam/src/view/main/Main.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import styled from 'styled-components'
 import { Layout } from 'antd';
 import MenuSider from '../../components/MenuSider'
+import Headerbox from '../../components/Header'
 const { Header, Sider, Content } = Layout;
 
 export default function Main() {
   return <div className="Main"  >
     <Layout>
-      <Header style={{ background: '#fff' }}>Header</Header>
+      <Header style={{ background: '#fff' }}>
+        <Headerbox></Headerbox>
+      </Header>
       <Layout>
-        <Sider ><MenuSider></MenuSider></Sider>
+        <Sider><MenuSider></MenuSider></Sider>
         <Content>Content</Content>
       </Layout>
     </Layout>
