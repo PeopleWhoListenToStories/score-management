43742d5438c9a45b7c17fc3564f1318e8c31d2ba
zhengqiyue <2535250538@qq.com>
2020-08-24 13:55:44 +0800

    'exam'

--- view-exam/src/view/main/Main.tsx
+++ view-exam/src/view/main/Main.tsx
@@ -24,7 +24,7 @@ const getTitle = (path: string) => {
   menu.forEach(item => {
     item.children.forEach((value: any) => {
       if (value.path === path) {
-        title = value.meta.title;
+        title = value.meta.name;
       }
     })
   })
