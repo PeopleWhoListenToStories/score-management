2aa67e746b8fd292edcd736d334e06b3ce80d3bc
zero_an_two <1793023715@qq.com>
2020-08-26 08:55:27 +0800

    8-26

--- view-exam/src/utils/request.ts
+++ view-exam/src/utils/request.ts
@@ -5,7 +5,7 @@ import { getCookie } from "../utils/myCookie"
 
 const instance = axios.create({
   timeout: 1000,
-  baseURL: 'http://127.0.0.1/:7002'
+  baseURL: 'http://120.53.2.185'
 })
 
 instance.interceptors.request.use((request: any) => {
