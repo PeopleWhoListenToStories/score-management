e7bec009e333b97152155c895c0559f47ee03815
zhengqiyue <2535250538@qq.com>
2020-08-28 09:50:01 +0800

    's'

--- view-exam/src/store/modules/language/index.ts
+++ view-exam/src/store/modules/language/index.ts
@@ -2,10 +2,12 @@ import { observable, action } from 'mobx'
 import JP from '../../../langauge/JP'
 import zhCN from "../../../langauge/CN"
 import enUS from "../../../langauge/US"
+import kr from "../../../langauge/KR"
 const message = {
   'en': enUS,
   'zh': zhCN,
-  'jp': JP
+  'jp': JP,
+  'kr':kr
 }
 
 class Language {
